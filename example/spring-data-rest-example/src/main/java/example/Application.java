f48a8eb2fe49458bf6ee8514f20657bc606461bf
cproinger <christian.proinger@gmail.com>
2015-08-29 22:30:01 +0200

    added chincook sample database as embedded h2-database

--- example/spring-data-rest-example/src/main/java/example/Application.java
+++ example/spring-data-rest-example/src/main/java/example/Application.java
@@ -3,11 +3,16 @@ package example;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 
 //@EnableJpaRepositories
@@ -17,6 +22,16 @@ public class Application extends RepositoryRestMvcConfiguration {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class);
 		assert context != null;
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder()
+			.setType(EmbeddedDatabaseType.H2)
+//			.addScript("classpath:sql/Chinook_Sqlite.sql")
+			.addScript("classpath:sql/create-tables.sql")
+			.addScript("classpath:sql/insert-data.sql")
+			.build();
 	}
 	
     @Override
